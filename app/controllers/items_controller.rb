0466c74b81563769f88f46529f9a90200d6d0627
root <root@LAPTOP-78CKJOIP.localdomain>
2022-02-07 12:02:12 -0600

    commit tree 82b4d9e54de805c7f4200db9ec0131d4250c1807

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,41 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
+    #byebug
+    params[:user_id] ?
+    items = User.find(params[:user_id]).items :
     items = Item.all
-    render json: items, include: :user
+    render json: items, include: :user 
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    params[:user_id] ?
+    item = Item.create(item_params):
+    null
+    
+ 
+    User.find(params[:user_id]).items << item 
+    render json: item, include: :user, status: :created
+  end
+
+  private
+
+  def record_not_found
+    render json: { errors: "record not found" }, status: :not_found
+  end
+
+  def unprocessable
+    render json: { errors: "unprocessable" }, status: :unprocessable_entity
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
 
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   def show
-    user = User.find_by(id: params[:id])
     render json: user, include: :items
+  end
+
+
+  private 
+
+  def user
+    User.find(params[:id])
+  end
+
+  def record_not_found
+    render json: { error: "record not found" }, status: :not_found 
   end
 
 end
